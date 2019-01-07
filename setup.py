4841c91638a99a960e609706019c3435cac9c5b8
Bishaka Samuel <sbishaka@gmail.com>
2019-01-07 18:14:10 +0300

    bump module version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tdctlauthcard",
-    version="0.0.7",
+    version="0.0.8",
     author="Bishaka Samuel",
     author_email="sbishaka@gmail.com",
     description="Auth utility",
