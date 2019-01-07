e97bd2b73beac5735ed8640f1c90972498a1f1e9
Bishaka Samuel <sbishaka@gmail.com>
2019-01-07 17:18:02 +0300

    bump module version to 0.0.3 to test travis deploy to test pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tdctlauthcard",
-    version="0.0.2",
+    version="0.0.3",
     author="Bishaka Samuel",
     author_email="sbishaka@gmail.com",
     description="Auth utility",
