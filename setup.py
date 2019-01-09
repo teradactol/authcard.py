2f4c215c8d18a9bf606d7c98346edafeedfa3163
Bishaka Samuel <sbishaka@gmail.com>
2019-01-09 11:56:19 +0300

    prepare for release of 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tdctlauthcard",
-    version="0.0.12",
+    version="0.0.13",
     author="Bishaka Samuel",
     author_email="sbishaka@gmail.com",
     description="Auth utility",
