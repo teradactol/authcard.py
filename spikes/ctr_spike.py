9178f5de9d7b04e0e282db8a4c7b97d18275156f
Bishaka Samuel <sbishaka@gmail.com>
2019-01-11 14:24:00 +0300

    add authcard.encryptutils.ctr.encryptText

--- spikes/ctr_spike.py
+++ spikes/ctr_spike.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto.Util.number import bytes_to_long
+from Crypto.Random import get_random_bytes
 
 delim = ":"
 
@@ -24,3 +25,23 @@ def decryptText(opts={}):
     decrypted_text = decrypted_bytes.decode("utf-8")
 
     return decrypted_text
+
+def encryptText(opts={}):
+    text = opts.get('text',"")
+    text_bytes = bytes(text,'utf-8')
+    
+    passwd = opts.get('pass',"")
+    passwd_bytes = bytes(passwd,'utf-8')
+
+    iv_bytes = get_random_bytes(16)
+    iv_hex = iv_bytes.hex()
+
+    counter = Counter.new(128, initial_value = bytes_to_long(iv_bytes))
+
+    cipher = AES.new(passwd_bytes, AES.MODE_CTR, counter = counter)
+
+    encrypted_bytes = cipher.encrypt(text_bytes)
+    encrypted_hex = encrypted_bytes.hex()
+
+    hash = iv_hex + delim + encrypted_hex
+    return hash
--- spikes/encrtyputils_ctr_spike.py
+++ spikes/encrtyputils_ctr_spike.py
@@ -10,7 +10,13 @@ import authcard.encryptutils.ctr
 import authcard.encryptutils.bcrypt
 
 passwd = "S62Sb.[RXFD#QCRx7n63A*C:ZzkaS7N;"
+text = "My Text"
 hash = "65e68599e50a287e9cff5dbe5818af14:653fecf2c33bc6"
-decrypted_text = authcard.encryptutils.ctr.decryptText({"hash":hash,"pass":passwd})
 
+hash_2 = authcard.encryptutils.ctr.encryptText({"text":text,"pass":passwd})
+decrypted_text = authcard.encryptutils.ctr.decryptText({"hash":hash,"pass":passwd})
+decrypted_text_2 = authcard.encryptutils.ctr.decryptText({"hash":hash_2,"pass":passwd})
+
+print(hash_2)
 print(decrypted_text)
+print(decrypted_text_2)
